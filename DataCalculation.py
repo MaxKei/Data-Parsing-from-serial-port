9895ecc559236ef145c6019b9b152c6f0378389d
Max <90686794+KushMax@users.noreply.github.com>
2021-12-27 12:53:22 +0100

    Update DataCalculation.py

--- DataCalculation.py
+++ DataCalculation.py
@@ -7,7 +7,6 @@ from datetime import datetime
 
 def SaveDeviceCountToTXT():
     count = 0
-
     # output_dictionary = {'ID': 'Last Seen'}
     output_dictionary = {}
 
@@ -19,16 +18,17 @@ def SaveDeviceCountToTXT():
             lastSeen_dateTime = datetime.strptime(lastSeen_str, "%m/%d/%Y, %H:%M:%S")
             output_dictionary[ID] = lastSeen_dateTime
 
+    # All printstatements used only for the console, not a necessity for the program
     sorted(output_dictionary)
     print()
     print('List of devices seen within', timeout_seconds, 'seconds:')
     print()
     for key in output_dictionary:
         print(key, 'was seen nearby')
-        count = count + 1
+        count = count + 1 # everytime an key/ID was found, count up.
     print()
     print(count, 'unique devices pinged esp32 in the last', timeout_seconds, 'seconds.')
     print()
-
+    # Open the txt document, and write inside as a string.
     with open('device_count.txt', 'w') as f:
         f.write(count.__str__());
